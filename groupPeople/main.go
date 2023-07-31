083a828839573ef6bf8b8a431dae0c1efbfd4797
Yogesh Gophane <ygophane2@gmail.com>
2023-07-31 22:32:00 +0530

    adding new changes

---
+++ groupPeople/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	input := []int{4, 5, 6, 7, 0, 2, 1, 3}
+
+	np := input[2:4]
+	go restoreString()
+
+	// select {}
+
+	fmt.Print(cap(np))
+
+}
+
+// func groupThePeople(groupSizes []int) [][]int {
+//     var res  = make(string[int],10)
+
+// }
+
+func restoreString() {
+	fmt.Print("hello yogesh")
+
+}
